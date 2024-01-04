b263f68a159f3f8f4c54e8e16489128d58fdd6e2
Vitoria F <vitoria.s.franca@gmail.com>
2024-01-04 10:38:22 -0300

    configurando o projeto

---
+++ src/main/java/com/generation/Farmacia_Bem_Estar/FarmaciaBemEstarApplication.java
@@ -0,0 +1,13 @@
+package com.generation.Farmacia_Bem_Estar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaBemEstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaBemEstarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/Farmacia_Bem_Estar/FarmaciaBemEstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.Farmacia_Bem_Estar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaBemEstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
